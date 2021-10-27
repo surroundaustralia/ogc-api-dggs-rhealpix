fa47fd4e0b5aba8725fb1f2f6dcab8a75560f4b6
david-habgood <david.habgood@surroundaustralia.com>
2021-10-27 15:38:33 +1000

    Added basic homepage Add autogenerated requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from .routers import dggsZQQuery, dggsZQSet
 
@@ -7,6 +10,7 @@ app.include_router(dggsZQSet.router)
 app.include_router(dggsZQQuery.router)
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    html_content = Path("app/static/home.html").read_text()
+    return HTMLResponse(content=html_content, status_code=200)
